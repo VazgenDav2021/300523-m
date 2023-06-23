aca381c84e740c4e996344f94430961004428bb2
Vazgen <d.2100.v@gmail.com>
2023-06-23 14:48:21 +0400

    push

---
+++ 23.06.2023/anserToQuestion/index.js
@@ -0,0 +1,17 @@
+// Loop
+// здесь обращаюсть к DOMу и привязываюсь к элементу .container
+// const container = document.querySelector(".container");
+
+// const randomColorGenerator = () =>
+//   `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(
+//     Math.random() * 255
+//   )}, ${Math.floor(Math.random() * 255)})`;
+
+// for (let i = 0; i < 10; i++) {
+//   const newDiv = document.createElement("div");
+//   newDiv.style.width = "500px";
+//   newDiv.style.height = "500px";
+//   newDiv.style.backgroundColor = randomColorGenerator();
+
+//   container.append(newDiv);
+// }

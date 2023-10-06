b43848e3cebd3eba962eee3cd83571b1ea43ece7
Vazgen <d.2100.v@gmail.com>
2023-10-06 14:47:54 +0400

    push

--- 06.10.2023/array.js
+++ 06.10.2023/array.js
@@ -103,3 +103,110 @@ const people = [
 // 4 Создать новыю переменную которая ровна сумме всех зп (reduce)
 // 5 Создать 2 переменные в которых хранятся маассыв с пользвоталми горадмами Москва и Новосибирск
 // 6 Создать новую переменную которая ровна массивы из 2 масиввов городов (пунк 5)
+
+// Liliia
+// const sortBySalary = people.sort((a, b) => a.salary - b.salary)
+// const findAge = people.find(el => el.age === 27)
+// const nameAndAgeArray = people.map(({ name, age }) => {
+//     return {
+//         name,
+//         age
+//     }
+// })
+// const sumOfsalary = people.reduce((accumulator, el) => {
+//     return accumulator + el.salary
+// }, 0)
+// const novosibCity = people.filter(el => el.city === 'Новосибирск')
+// const mosсowCity = people.filter(el => el.city === 'Москва')
+// const twoCity = novosibCity.concat(mosсowCity)
+
+
+// Laziza
+// const mainSalary = people.sort((a, b) => a.salary - b.salary)
+// const person = people.find(person => person.age === 27)
+// const newMa = people.map(person => ({ name: person.name, age: person.age }))
+// const sumOfSallary = people.reduce((accumulator, currentPerson) => {
+//     return accumulator + currentPerson.salary;
+// }, 0);
+// const moscow = people.filter(person => person.city === "Москва")
+// const novosibirsk = people.filter(person => person.city === "Новосибирск")
+// const twoCities = moscow.concat(novosibirsk)
+// const twoCitie2 = [...moscow, ...novosibirsk]
+
+// Shafayat
+// const filterSalary = people.sort((a, b) => a.salary - b.salary);
+// const age27 = people.find((el) => el.age === 27)
+// const nameAge = people.map(({ name, age }) => {
+//     return {
+//         name, age
+//     }
+// });
+// const sumSalary = people.reduce((accumulator, el) => {
+//     return accumulator + el.salary;
+// }, 0)
+// const cityMoskva = people.filter((el) =>  el.city === "Москва" )
+// const cityNovosibirsk = people.filter((el) =>  el.city === "Новосибирск" )
+// const twoCity = [...cityMoskva, ...cityNovosibirsk]
+
+// Ulyana
+// const people2 = [
+//     {
+//         name: "Иван",
+//         age: 30,
+//         city: "Mainz",
+//         salary: 50000
+//     },
+//     {
+//         name: "Анна",
+//         age: 25,
+//         city: "Санкт-Петербург",
+//         salary: 45000
+//     },
+//     {
+//         name: "Мария",
+//         age: 35,
+//         city: "Mainz",
+//         salary: 60000
+//     },
+//     {
+//         name: "Петр",
+//         age: 28,
+//         city: "Baden",
+//         salary: 48000
+//     },
+//     {
+//         name: "Елена",
+//         age: 27,
+//         city: "Baden",
+//         salary: 52000
+//     }
+// ];
+// const sortedBySalary = people.sort((a, b) => a.salary - b.salary);
+// const age27 = people.find(person => person.age === 27);
+// const nameAge = people.map(person => ({ name: person.name, age: person.age }));
+// const totalSalary = people.reduce((accumulator, person) => accumulator + person.salary, 0);
+// const usersInMainzAndBaden = people2.filter(person => person.city === 'Mainz' || person.city === 'Baden');
+
+// Anna
+// const sortedPeople = people.sort((a, b) => a.salary - b.salary)
+// const elAge = people.find(el => el.age === 27)
+// const newPeople = people.map(person => ({ name: person.name, age: person.age }))
+// const sumOfSalary = people.reduce((accumulator, el) => {
+//     return accumulator + el.salary;
+// }, 0)
+// const moscow = people.filter(el => el.city === 'Москва')
+// const novosibirsk = people.filter(el => el.city === 'Новосибирск')
+// const cities = moscow.concat(novosibirsk)
+// const twocities = [...moscow, ...novosibisrk]
+
+// Natalia
+// const sortedBySalary = people.sort((a, b) => a.salary - b.salary);
+// const elemWithAge27 = people.find(pers => pers.age === 27);
+// const nameAndAge = people.map(({ name, age }) => ({ name, age }));
+// const totalSalary = people.reduce((accumulator, pers) => accumulator + pers.salary, 0);
+// const personLiveInMoskow = people.filter((pers) => pers.city === "Москва");
+// const personLiveInNovosibirsk = people.filter(
+//     (pers) => pers.city === "Новосибирск"
+// );
+// const twoCitie2 = personLiveInMoskow.concat(personLiveInNovosibirsk);
+// const twoCitie3 = [...personLiveInMoskow, ...personLiveInNovosibirsk];

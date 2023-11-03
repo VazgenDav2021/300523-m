c1be04c8723aa78e0dc36a1809883af728eabc44
Vazgen <d.2100.v@gmail.com>
2023-11-03 15:40:46 +0400

    push

--- 03.11.2023/index.js
+++ 03.11.2023/index.js
@@ -54,25 +54,54 @@
 // }
 
 
-const promises3 = [
-    fetch('https://jsonplaceholder.typicode.com/posts'),
-    fetch('https://jsonplaceholder.typicode.com/posts/1'),
-    fetch('https://jsonplaceholder.typicode.com/notexist'),
-]
+// const promises3 = [
+//     fetch('https://jsonplaceholder.typicode.com/posts'),
+//     fetch('https://jsonplaceholder.typicode.com/posts/1'),
+//     fetch('https://jsonplaceholder.typicode.com/notexist'),
+// ]
 
 
-Promise.race(promises3).then((response=>{
-    if(response.ok){
-        return response.json()
-    } else {
-        throw new Error ("Request failed")
-    }
-})).then(data=>{
-    console.log({data});
-})
+// Promise.race(promises3).then((response=>{
+//     if(response.ok){
+//         return response.json()
+//     } else {
+//         throw new Error ("Request failed")
+//     }
+// })).then(data=>{
+//     console.log({data});
+// })
 
 
 // 1. сделать функцию для простого запорса https://restcountries.com/v3.1/name/${динамичное название госудасртво} для получения информации о государстве
 // 2. Создать функцию которая получает данные о госудасстве и их отрисовывает в html (флаг, название, валюта, население, столица)
 // 3. Вызвать функция для получения государства (пункт 1) в обработчике события DOMContentLoaded
 
+
+const body = document.body
+const renderCurrentCountry = (countryInfo) => {
+    const { flags: { png }, capital, currencies, population, name: { official } } = countryInfo
+    console.log({ currencies });
+    const countryCard = document.createElement("div")
+    countryCard.classList.add("card")
+    const currencyValue = Object.keys(currencies)[0]
+    const html = `
+        <img class="${official}-card" src="${png}" alt="${official}"/>
+        <p>${official}</p>
+        <p>population: ${population}</p>
+        <p>capital: ${capital[0]}</p>
+        <p>currencies: ${currencies[currencyValue].name}</p>
+    `
+    countryCard.insertAdjacentHTML("afterbegin", html)
+    body.append(countryCard)
+}
+
+
+const getCountryInfo = async (countryName) => {
+    const countryInfoArray = await fetch(`https://restcountries.com/v3.1/name/${countryName}`).then(res => res.json())
+    const [countryinfo] = countryInfoArray;
+    renderCurrentCountry(countryinfo)
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    getCountryInfo("ukraine")
+})

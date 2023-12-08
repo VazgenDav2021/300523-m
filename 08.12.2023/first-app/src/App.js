4f9f5662b38e490f15dfbdbf5e202776761b11bd
Vazgen <d.2100.v@gmail.com>
2023-12-08 14:07:01 +0400

    push

--- 08.12.2023/first-app/src/App.js
+++ 08.12.2023/first-app/src/App.js
@@ -1,11 +1,27 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+
+
+// function App является компонентом который возращает JSX(HTML)
+// function App() {
+//   // внутри компонента, до ключевого слово "return" мы можем использовать полнацненый JS, создавать функции, переменные, длеать условные вычисления и тд
+//   const name = "Vazgen"
+//   return (
+//     <div className="App">
+//       Hello World, {name}
+//     </div>
+//   );
+// }
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Navbar />
+      <div className='container'></div>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ 08.12.2023/first-app/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+// rafce
+import React from 'react'
+import './index.css'
+
+const Navbar = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Settings</li>
+        </ul>
+    )
+}
+
+export default Navbar;

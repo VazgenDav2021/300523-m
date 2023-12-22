dd652bf8e2a8f32aa1056741a094532df804233c
Vazgen <d.2100.v@gmail.com>
2023-12-22 23:13:06 +0400

    push

--- 22.12.2023/src/App.js
+++ 22.12.2023/src/App.js
@@ -2,6 +2,18 @@ import './App.css';
 import Janis from './components/Yanis/Yanis';
 import Ulyana from './components/Ulyana/Ulyana';
 
+// 1 Создать состояние "ползователи"
+// 2 Сделать запрос на https://jsonplaceholder.typicode.com/users
+// 3 Обнавить состоянеи "ползователи" ответом который предет из сервера
+// 4 Отрисовать всех ползьвоателей которых получили
+// "name": "Leanne Graham",
+// "username": "Bret",
+// "email": "Sincere@april.biz",
+
+// Создаете новый проейт
+// Длеате запрос в компоненте App
+// с помощю map итеративно показываете компонент EachUser который должны создать
+
 function App() {
 
   return (
